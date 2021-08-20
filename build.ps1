cc9a6815675c6fc57dcbd2111e507c8d0b109169
Tyler VanBlargan <16465776+TerabyteTiger@users.noreply.github.com>
2021-08-20 13:44:15 -0400

    🔨 Build updates

--- build.ps1
+++ build.ps1
@@ -1,16 +1,16 @@
 # Html file compression
-html-minifier --collapse-whitespace index.html -o compressed\index.html
-html-minifier --collapse-whitespace game.html -o compressed\game.html
-html-minifier --collapse-whitespace notes.html -o compressed\notes.html
+html-minifier --collapse-whitespace index.html -o dist\index.html
+html-minifier --collapse-whitespace game.html -o dist\game.html
+html-minifier --collapse-whitespace notes.html -o dist\notes.html
 
 # CSS file compression
-csso main.css --output compressed\main.css
+csso main.css --output dist\main.css
 
 # js file compression
-terser game.js --compress --mangle --keep-fnames -o compressed\game.js
-terser webmon.js --compress --mangle -o compressed\webmon.js
+terser game.js --compress --mangle --keep-fnames -o dist\game.js
+terser webmon.js --compress --mangle -o dist\webmon.js
 
 # Copy Favicon
-Copy-Item ./favicon.ico ./compressed/favicon.ico
+Copy-Item ./favicon.ico ./dist/favicon.ico
 
 Write-Output "Compression complete"
